71b8b628d3042d6bdb7004938e51500283fdb69f
gophimtiensinh <minhthien2237@gmail.com>
2026-08-11 02:45:14 +0700

    add AI image detection service

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,5 +1,9 @@
 from src.api.controllers.todo_controller import bp as todo_bp
 from src.api.controllers.auth_controller import auth_bp as auth_bp
+from src.api.controllers.ai_detection_controller import bp as ai_detection_bp
+
+
 def register_routes(app):
     app.register_blueprint(todo_bp)
-    app.register_blueprint(auth_bp) 
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(ai_detection_bp)
