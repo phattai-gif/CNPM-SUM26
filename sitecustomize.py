aee25623f962ff8bf15f67143a025ccda5a19e59
gophimtiensinh <minhthien2237@gmail.com>
2026-08-11 20:13:24 +0700

    remove temporary sitecustomize workaround

--- sitecustomize.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-ROOT = os.path.dirname(os.path.abspath(__file__))
-SRC = os.path.join(ROOT, 'src')
-
-for path in (SRC, ROOT):
-    if path not in sys.path:
-        sys.path.insert(0, path)
